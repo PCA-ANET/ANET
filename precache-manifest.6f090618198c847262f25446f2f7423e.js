eb03295e275d8273eef3e8fa3426300eade40da9
Mohamed-Amine RAJA <MRAJA@pca.ma>
2019-09-23 09:56:50 +0100

    Updates

---
+++ precache-manifest.6f090618198c847262f25446f2f7423e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1f5b9b7ac518e2f40e75f3f2946a8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e751d639b58d719aba",
+    "url": "/static/css/6.4d178bd5.chunk.css"
+  },
+  {
+    "revision": "604ec96607182f772d1b",
+    "url": "/static/css/7.785c5571.chunk.css"
+  },
+  {
+    "revision": "edb825092be7a5282bc8",
+    "url": "/static/css/8.785c5571.chunk.css"
+  },
+  {
+    "revision": "11e5a03bf9668a848ade",
+    "url": "/static/js/0.37c7bb5f.chunk.js"
+  },
+  {
+    "revision": "b9cb1401051960de5b19",
+    "url": "/static/js/1.1cf494d7.chunk.js"
+  },
+  {
+    "revision": "e36adb12fb7ef9115b6e",
+    "url": "/static/js/4.c077c1b0.chunk.js"
+  },
+  {
+    "revision": "f60996ee9ef30633233f",
+    "url": "/static/js/5.d9168642.chunk.js"
+  },
+  {
+    "revision": "20e751d639b58d719aba",
+    "url": "/static/js/6.1363e907.chunk.js"
+  },
+  {
+    "revision": "604ec96607182f772d1b",
+    "url": "/static/js/7.8cdb511e.chunk.js"
+  },
+  {
+    "revision": "edb825092be7a5282bc8",
+    "url": "/static/js/8.c942a348.chunk.js"
+  },
+  {
+    "revision": "e1c5bea19b86def0e320",
+    "url": "/static/js/9.800bca91.chunk.js"
+  },
+  {
+    "revision": "474bcab15c55aff7e3e9",
+    "url": "/static/js/main.ab752410.chunk.js"
+  },
+  {
+    "revision": "e692c924e1b2dde9e013",
+    "url": "/static/js/runtime~main.c83ed5c8.js"
+  },
+  {
+    "revision": "08567b4497e9182d1c9051150233f886",
+    "url": "/static/media/logo-banqueatlantique-v.08567b44.svg"
+  },
+  {
+    "revision": "c5f44293fc5fe7c6389d6aaef2938171",
+    "url": "/static/media/logo.c5f44293.PNG"
+  }
+]);
