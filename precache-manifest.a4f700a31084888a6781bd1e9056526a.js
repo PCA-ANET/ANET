b352606849ea7647696f71b2d8865d7bf05b942e
Mohamed-Amine RAJA <MRAJA@pca.ma>
2019-09-23 10:11:34 +0100

    Updates

---
+++ precache-manifest.a4f700a31084888a6781bd1e9056526a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca4d62e75740f82851af5b462725c15",
+    "url": "/EBANKING/index.html"
+  },
+  {
+    "revision": "20e751d639b58d719aba",
+    "url": "/EBANKING/static/css/6.4d178bd5.chunk.css"
+  },
+  {
+    "revision": "604ec96607182f772d1b",
+    "url": "/EBANKING/static/css/7.785c5571.chunk.css"
+  },
+  {
+    "revision": "edb825092be7a5282bc8",
+    "url": "/EBANKING/static/css/8.785c5571.chunk.css"
+  },
+  {
+    "revision": "825bba31c1f3403141d4",
+    "url": "/EBANKING/static/js/0.8705e703.chunk.js"
+  },
+  {
+    "revision": "b9cb1401051960de5b19",
+    "url": "/EBANKING/static/js/1.1cf494d7.chunk.js"
+  },
+  {
+    "revision": "e36adb12fb7ef9115b6e",
+    "url": "/EBANKING/static/js/4.c077c1b0.chunk.js"
+  },
+  {
+    "revision": "e7948f3dd8de7792cfdf",
+    "url": "/EBANKING/static/js/5.02b974bf.chunk.js"
+  },
+  {
+    "revision": "20e751d639b58d719aba",
+    "url": "/EBANKING/static/js/6.1363e907.chunk.js"
+  },
+  {
+    "revision": "604ec96607182f772d1b",
+    "url": "/EBANKING/static/js/7.8cdb511e.chunk.js"
+  },
+  {
+    "revision": "edb825092be7a5282bc8",
+    "url": "/EBANKING/static/js/8.c942a348.chunk.js"
+  },
+  {
+    "revision": "e1c5bea19b86def0e320",
+    "url": "/EBANKING/static/js/9.800bca91.chunk.js"
+  },
+  {
+    "revision": "485b49bfee25242eb608",
+    "url": "/EBANKING/static/js/main.97397c31.chunk.js"
+  },
+  {
+    "revision": "2cc9c8698fc4dace472f",
+    "url": "/EBANKING/static/js/runtime~main.20bec7a4.js"
+  },
+  {
+    "revision": "08567b4497e9182d1c9051150233f886",
+    "url": "/EBANKING/static/media/logo-banqueatlantique-v.08567b44.svg"
+  },
+  {
+    "revision": "c5f44293fc5fe7c6389d6aaef2938171",
+    "url": "/EBANKING/static/media/logo.c5f44293.PNG"
+  }
+]);
